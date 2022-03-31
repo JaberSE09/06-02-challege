2b91399c1840ec04e5b43909b67b86f7ad420a1e
Sajid Jaber <sajidjaber@yahoo.com>
2022-03-31 12:03:53 -0500

    js started

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//global
+var cityText = document.getElementById("cityText")
+var searchBtn = document.getElementById("search")
+var recentSearch = document.getElementById("recentSearch")
+var currentWeather =document.getElementById("currentWeather")
+var Day5 =document.getElementById("5Day")
+
+function getCurrentCity(city) {
+    var apiUrl = ""
+}
