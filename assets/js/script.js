dd1ea81c271a78a6c7883dfd715088a45d1011da
Sajid Jaber <sajidjaber@yahoo.com>
2022-03-31 14:38:47 -0500

    started the fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,20 @@ var searchBtn = document.getElementById("search")
 var recentSearch = document.getElementById("recentSearch")
 var currentWeather =document.getElementById("currentWeather")
 var Day5 =document.getElementById("5Day")
-
+const key = "6d9f0ee409c3a4bbba290561246ccf25"
 function getCurrentCity(city) {
-    var apiUrl = ""
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+
+    fetch(apiUrl)
+        .then(function(response){
+
+            let data = response.json()
+            return data
+        })
+        .then(function(data){
+            console.log(data)
+
+        })
 }
+
+getCurrentCity("milwaukee")
