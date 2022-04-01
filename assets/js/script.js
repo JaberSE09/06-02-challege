66bd57c4607ed1113b7f9e726d2f8578dbc88529
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-01 15:15:04 -0500

    set the title up

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,11 @@ var searchBtn = document.getElementById("search")
 var recentSearch = document.getElementById("recentSearch")
 var currentWeather =document.getElementById("currentWeather")
 var Day5 =document.getElementById("5Day")
+var weather = document.getElementById("weather")
 const key = "6d9f0ee409c3a4bbba290561246ccf25"
+var time = moment().format('(M/D/Y)');
+var titleEl= document.createElement("h1")
+
 function getCurrentCity() {
     let city = cityText.value.trim()
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
@@ -17,12 +21,19 @@ function getCurrentCity() {
         })
         .then(function(data)
         {
-            let title = document.createElement("h1")
-            title.innerHTML=data.name
-            currentWeather.appendChild(title)         
-            console.log(data.name)
+            if(titleEl.innerHTML){
+                currentWeather.removeChild(titleEl)
+            }
+            weather.style.border="2px solid black"
+            titleEl = document.createElement("h1")
+            titleEl.innerHTML = data.name + time
+            currentWeather.appendChild(titleEl)
+            console.log(data)
 
 
+        })
+        .catch(function(error){
+            console.log(error)
         })
 }
 
