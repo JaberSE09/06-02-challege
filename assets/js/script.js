fba9cab6b67a5674a10e93683e0b0d096612160c
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-02 14:51:15 -0500

    chaged the getcurrentcity to have a param

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,59 +8,64 @@ var weather = document.getElementById("weather")
 const key = "6d9f0ee409c3a4bbba290561246ccf25"
 var time = moment().format('(M/D/Y)');
 
-function getCurrentCity() {
-    let city = cityText.value.trim()
+function getCurrentCity(city) {
+    currentWeather.replaceChildren()
+    currentWeather.style.border="2px solid black"
+
+
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`
 
     fetch(apiUrl)
         .then(function (response) {
 
-            let data = response.json()
-            return data
+            return response.json()
 
         })
         .then(function (data) {
 
 
-            const oneCallUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${key}`
-            var icon = document.createElement("img")
-            icon.setAttribute("src" , "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
-            
+            const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=${key}`
             var titleEl = document.createElement("h1")
-            titleEl.style.fontWeight="bold"
-            
-            var tempEl = document.createElement("p")
-            var windEl = document.createElement("p")
-            var humEl = document.createElement("p")
-            var uvEl = document.createElement("p")
-            
-            weather.style.border = "2px solid black"
-            
+            titleEl.style.fontWeight = "bold"
+
             titleEl.innerHTML = data.name + time
+
+
+            var icon = document.createElement("img")
+            icon.setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
             titleEl.appendChild(icon)
-            
-            windEl.innerHTML= "wind: " + data.wind.speed + " MPH"
-
-            humEl.innerHTML ="Humidity: " + data.main.humidity + " %"
-
-            uvEl.innerHTML= "UV Index: " 
-
-            tempEl.innerHTML ="Temp: " + data.main.temp+ "°F"
             currentWeather.appendChild(titleEl)
-            currentWeather.appendChild(tempEl)
-            currentWeather.appendChild(windEl)
-            currentWeather.appendChild(humEl)
-            currentWeather.appendChild(uvEl)
-            console.log(data)
 
             fetch(oneCallUrl)
-            .then(function(response){
-                response.json()
-                return response
-            })
-            .then(function(data){
-                console.log(data)
-            })
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+
+
+
+                    var tempEl = document.createElement("p")
+                    var windEl = document.createElement("p")
+                    var humEl = document.createElement("p")
+                    var uvEl = document.createElement("p")
+
+
+                    tempEl.innerHTML ="Temp: " + Math.floor(data.current.temp)+ "°F"
+
+                    // windEl.innerHTML= "wind: " + data.wind.speed + " MPH"
+
+                    // humEl.innerHTML ="Humidity: " + data.main.humidity + " %"
+
+                    // uvEl.innerHTML= "UV Index: " 
+
+                    currentWeather.appendChild(tempEl)
+                    // currentWeather.appendChild(windEl)
+                    // currentWeather.appendChild(humEl)
+                    // currentWeather.appendChild(uvEl)
+                    console.log(data)
+
+                    console.log(data)
+                })
 
 
         })
@@ -69,8 +74,10 @@ function getCurrentCity() {
         })
 }
 
-searchBtn.addEventListener("click",function() {
-    currentWeather.replaceChildren()
-    getCurrentCity()
+
+getCurrentCity("San Diego")
+
+searchBtn.addEventListener("click", function () {
+    getCurrentCity(cityText.value.trim())
 }
-    )
+)
